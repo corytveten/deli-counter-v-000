b6fa6e866d89818c88338edfd6ad9f7c66e611fd
corytveten <corytveten@gmail.com>
2018-07-02 14:12:57 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -41,7 +41,7 @@ def line(katz_deli)
   if katz_deli = []
     puts "The line is currently empty."
   else
-    katz_deli.each {|person|
-     puts "The line is currently #{person}."}
+    katz_deli.each {|index, person|
+     puts "The line is currently #{index}. #{person}."}
   end
 end
